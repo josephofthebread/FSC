3012279d5925b64c6657c6ab9bbae3ed842f8f69
Mike Redko <mike.redko49@gmail.com>
2022-02-11 12:14:40 +0300

    Update source.cpp

--- src/source.cpp
+++ src/source.cpp
@@ -2,5 +2,5 @@
 #include ARTIFACT
 
 int Vkapp STOP auto FINISH END printf("wtf\n") STOP
-"hello %s\n" CUM "world" START BEGIN void main STOP
+"hello %s\n" FSC "world" START BEGIN void main STOP
 int START END Vkapp STOP puts(itoa(0)) START BEGIN;
